9e5d378bfd4a1376a1f5759d3db24270d44cc2b6
ryan.oxales2003 <ryan.oxales2003@gmail.com>
2025-07-29 12:00:34 +0800

    recaptch debug

--- src/components/6ContactsComponent.vue
+++ src/components/6ContactsComponent.vue
@@ -141,7 +141,7 @@ const submitForm = async () => {
  * 
  */
 
-const SITE_KEY = '6LcA2JIrAAAAAIHjU0ofUlrELFvfynzrN8iNDkdU';  // Replace with your site key
+const SITE_KEY = '6Ldv2JIrAAAAAJMaQbUQKzjV5ir4bZrCebAeX3S7';  // Replace with your site key
 
 const recaptchaContainer = ref(null);
 const recaptchaWidgetId = ref(null);
